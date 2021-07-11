674e3a50d988ff2d9fa80d9e78a7aaa3c4e806c6
czelijm <czelijm@outlook.com>
2021-07-12 01:33:13 +0200

    modified album item style

--- sq-react/src/components/album-item/album-item.component.jsx
+++ sq-react/src/components/album-item/album-item.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import {AlbumItemComponentStyled,Image, Title} from './album-item.styles';
+import {AlbumItemComponentStyled,Image, Title,StreamingMediaDiv, Anchor, PlatformIcon, DetailsButton,ButtonDiv} from './album-item.styles';
 
 const AlbumItem = ({album}) => {
 
-    const {id, title,image} = album;
+    const {id, title,image,iconArray,mediaPlatformsArray} = album;
 
     return(
         <AlbumItemComponentStyled>
             <Image src={image}/>
-            <Title>title</Title>
+            <Title>{title}</Title>
+            <StreamingMediaDiv>{iconArray.map((icon,index)=><Anchor href={mediaPlatformsArray[index]} ><PlatformIcon src={icon} alt="" /></Anchor>)}</StreamingMediaDiv>
+            <ButtonDiv>
+                <DetailsButton style={{"width":"100%"}}>Obczai szczegóły!</DetailsButton>
+            </ButtonDiv>
         </AlbumItemComponentStyled>
     );
 }
--- sq-react/src/components/album-item/album-item.styles.jsx
+++ sq-react/src/components/album-item/album-item.styles.jsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
+import Button from '../button/button.component';
 
 export const AlbumItemComponentStyled = styled.div`
-    background-color:gray;
+    padding:1%;
+    background-color:#2b303a;
     display:inline-flex;
     flex-direction: column;
     margin-left:3%;
     margin-top:3%;
+    border-radius:5px;
     @media only screen and (max-width: 991px){
         /* display:flex; */
         margin-left:0%;
@@ -24,7 +27,31 @@ export const Image = styled.img`
     display:block;
 `;
 
+export const PlatformIcon = styled.img`
+    width:33px;
+    height:auto;
+    padding:5px;
+`;
+
 export const Title = styled.h2`
     display:block;
 `;
 
+export const StreamingMediaDiv = styled.div`
+`;
+
+export const ButtonDiv = styled.div`
+    /* padding:5px; */
+    padding-top:5px;
+    width:100%;
+`;
+
+export const Anchor = styled.a`
+`;
+
+export const DetailsButton = styled(Button)`
+    /* width:10%; */  
+    display:flex;
+    align-self: center;
+    width:100% !important;
+`;
