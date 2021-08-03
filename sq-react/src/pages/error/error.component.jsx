633ec476bd0aa0660f176eeb8ee7e8efff7ba75d
czelijm <czelijm@outlook.com>
2021-08-03 22:25:37 +0200

    added error page

--- sq-react/src/pages/error/error.component.jsx
+++ sq-react/src/pages/error/error.component.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import ReactPlayer from 'react-player'
+import {withStyles} from '@material-ui/styles'
+import SomethingWentWrong from '../../assets/404.mp4'
 
-const ErrorComponent = () => (
-    <div>Error</div>
+
+const styles = {
+    title: {
+        'text-align':'center'
+    },
+  };
+
+const ErrorComponent = ({classes}) => (
+    <div>
+        <h2 className={classes.title}>Coś poszło nie tak...</h2>
+        <ReactPlayer 
+            playing
+            url={SomethingWentWrong}
+            width='100%'
+            height='100%'
+            loop
+            muted
+        />
+    </div>
 )
 
-export default ErrorComponent;
+export default withStyles(styles)(ErrorComponent);
